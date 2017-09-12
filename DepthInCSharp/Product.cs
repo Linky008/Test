82f1d60afd4cb6829af7ad4081caf79e57451d44
Linky008 <zlj1230504@sina.com>
2017-09-12 21:53:26 +0800

    c#小练习

---
+++ DepthInCSharp/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace DepthInCSharp
+{
+    /// <summary>
+    /// c#1
+    /// </summary>
+    public class Product
+    {
+        string name;
+        public string Name { get { return name; } }
+
+        decimal price;
+        public decimal Price { get { return price; } }
+
+        public Product(string name, decimal price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public static List<Product> GetSampleProducts()
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("West Side Story",9.99m));
+            list.Add(new Product("Assassins", 14.99m));
+            list.Add(new Product("Frogs", 13.99m));
+            list.Add(new Product("Sweeney Todd", 10.99m));
+            return list;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}:{1}",name,price);
+        }
+
+
+    }
+}
---
+++ DepthInCSharp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DepthInCSharp
+{
+
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+
+
+        
+            Console.WriteLine(("a\b/c/d/e").Length.ToString());
+            Console.Read();
+        }
+    }
+
+    public class Node
+    {
+        public Object Element;
+        public Node Link;
+        public Node()
+        {
+            Element = null;
+            Link = null;
+        }
+
+        public Node(object theElement)
+        {
+            Element = theElement;
+            Link = null;
+        }
+    }
+
+    public class LinkedList
+    {
+        protected Node header;
+        public LinkedList()
+        {
+            header = new Node("header");
+        }
+    }
+    
+}
